b7d7f38894e89df19080a208142f4150ac8dab73
tombolton96 <tbolton96@icloud.com>
2024-11-02 20:44:28 +0000

    update car to use AI control type

--- car.js
+++ car.js
@@ -20,8 +20,13 @@ class Car {
         this.angle = 0;
         this.damaged = false;
 
+        this.useBrain = controlType === "AI";
+
         if (controlType !== "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+              [this.sensor.rayCount, 6, 4]
+            );
         }
         this.controls = new Controls(controlType);
     }
@@ -38,6 +43,17 @@ class Car {
         }
         if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(
+              s => s === null ? 0 : 1 - s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
@@ -114,7 +130,7 @@ class Car {
         }
 
         // left right movement
-        if (this.speed != 0) {
+        if (this.speed !== 0) {
             const flip = this.speed > 0 ? 1 : -1;
             if (this.controls.left) {
                 this.angle += 0.03 * flip;
